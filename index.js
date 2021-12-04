63158f1645a1ba39ef1d85f68032871b83322cf4
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2021-12-04 14:18:35 +0200

    j

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
 app.get('/', (req, res) => {
   restaurant_model.getRestaurants()
   .then(response => {
-    console.log(res.json({message: "hello"}))
+    console.log("response: "+JSON.stringify(response))
     res.status(200).send(response);
   })
   .catch(error => {

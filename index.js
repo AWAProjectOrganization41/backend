7b27fe16385cecae48da46f3277859717d2d9e46
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2021-12-04 14:43:45 +0200

    h

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require('path')
 app.use(express.json())
 
   app.use(express.static(path.join(__dirname, 'build')))
-  app.get('/*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   })
 

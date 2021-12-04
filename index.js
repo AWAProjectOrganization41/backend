b9e8145d65feff8a890a18c7a9d1bc8037d8d5d5
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2021-12-04 15:34:32 +0200

    d

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const path = require('path')
 app.use(express.json())
 
   app.use(express.static(path.join(__dirname, 'build')))
-  /*app.get('/', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  })*/
+  })
 
 const restaurant_model = require('./restaurant_model')
 

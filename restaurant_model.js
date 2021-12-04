5f38009eb1336492e35b53c9ca68f84d1df6cacf
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2021-12-04 13:52:09 +0200

    client

--- restaurant_model.js
+++ restaurant_model.js
@@ -1,15 +1,16 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'local_u',
-  host: 'localhost',
-  database: 'restaurant_db',
-  password: '1234',
-  port: 5432,
+const { client } = require('pg')
+const client = new client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+}
 });
+
+client.connect()
 
 const getRestaurants = () => {
     return new Promise(function(resolve, reject) {
-      pool.query('SELECT * FROM restaurant', (error, results) => {
+      client.query('SELECT * FROM restaurant', (error, results) => {
         if (error) {
           reject(error)
         }
@@ -20,18 +21,20 @@ const getRestaurants = () => {
   const createRestaurant = (body) => {
     return new Promise(function(resolve, reject) {
       const { name, address, operating_hours, imagePath, restaurantType, priceLevel } = body
-      pool.query('INSERT INTO restaurant (name, address, operating_hours, imagepath, restaurant_type, price_level) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [name, address, operating_hours, imagePath, restaurantType, priceLevel], (error, results) => {
+      client.query('INSERT INTO restaurant (name, address, operating_hours, imagepath, restaurant_type, price_level) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [name, address, operating_hours, imagePath, restaurantType, priceLevel], (error, results) => {
         if (error) {
           reject(error)
         }
+        console.log(JSON.stringify(row))
         resolve(`A new restaurant has been added added: ${results.rows[0]}`)
+        client.end()
       })
     })
   }
   const deleteRestaurant = () => {
     return new Promise(function(resolve, reject) {
       const id = parseInt(request.params.id)
-      pool.query('DELETE FROM restaurant WHERE id = $1', [id], (error, results) => {
+      client.query('DELETE FROM restaurant WHERE id = $1', [id], (error, results) => {
         if (error) {
           reject(error)
         }

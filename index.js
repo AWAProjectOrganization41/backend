840adb701c884253320f5fdab424b6f75e677744
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2021-12-05 17:46:08 +0200

    jkh

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3001
 
 const restaurant_model = require('./restaurant_model')
 
-app.use(express.json())
+//app.use(express.json())
 
 // Kommaa jos teet lokaalisti:
 app.use(express.static(path.join(__dirname, 'build')))

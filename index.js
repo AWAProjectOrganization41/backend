38a14feff69788502cfefe56ff33be16aecfa760
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2021-12-14 15:08:07 +0200

    sdasd

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'build')))
   })
 
   app.use(express.static(path.join(__dirname, 'build')))
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   })
 

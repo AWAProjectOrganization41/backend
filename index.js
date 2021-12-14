9ff7e54c6138b6c96e59d287d33d2782bd8a8b45
t0klsa00 <80824045+t0klsa00@users.noreply.github.com>
2021-12-14 15:05:21 +0200

    dc

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 // Kommaa jos teet lokaalisti:
 app.use(express.static(path.join(__dirname, 'build')))
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   })
 

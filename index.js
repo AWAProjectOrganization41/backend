ac49d2e0a030c3b2391177625b8098f695c994eb
veerakatariin <t0pive00@students.oamk.fi>
2021-12-14 12:39:10 +0200

    old functions deleted

--- index.js
+++ index.js
@@ -135,39 +135,6 @@ app.post('/create_restaurant_login', (req, res) => {
   })
 })
 
-app.delete('/restaurant/:id', (req, res) => {
-  restaurant_model.deleteRestaurant(req.params.id)
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    res.status(500).send(error);
-  })
-})
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
-/*
-app.get('/user_orderhistory', (req, res) => {
-  restaurant_model.getUserOrderhistory()
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    res.status(500).send(error);
-  })
-})
-
-app.get('/restaurant_orderhistory', (req, res) => {
-  restaurant_model.getRestaurantOrderhistory()
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    res.status(500).send(error);
-  })
-})*/
-
 app.post('/user_orderhistory', (req, res) => {
   restaurant_model.createUserOrder(req.body)
   .then(response => {
